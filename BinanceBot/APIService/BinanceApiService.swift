6aae852abfcd913715ad52ce97bc621242f7661f
Saman Badakhshan <sabobin@gmail.com>
2019-12-22 21:07:04 +0000

    added protocol and class skeliton for Binance API

---
+++ BinanceBot/APIService/BinanceApiService.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  BinanceBot
+//
+//  Created by Saman Badakhshan on 22/12/2019.
+//  Copyright © 2019 Saman Badakhshan. All rights reserved.
+//
+
+import Foundation
+
+protocol BinanceApiInterface
+{
+}
+
+class BinanceApiService: BinanceApiInterface
+{
+    
+}
